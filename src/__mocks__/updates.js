05e969b142551db70d26fce36dd425b747debe9f
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2017-05-13 20:04:32 +0200

    fix(flow): render connectors after end update is rendered
    Closes #10

--- src/__mocks__/updates.js
+++ src/__mocks__/updates.js
@@ -2,44 +2,27 @@ export default [
   {
     type: 'master',
     y: 50,
+    from: [],
     popup: true,
     content: 'Tag 0.1',
-    connectors: [
-      {
-        type: 'develop',
-        y: 100,
-      },
-      {
-        type: 'master',
-        y: 220,
-      },
-    ],
   },
   {
     type: 'develop',
     y: 100,
-    connectors: [
+    from: [
       {
-        type: 'develop',
-        y: 150,
+        type: 'master',
+        y: 50,
       },
     ],
   },
   {
     type: 'develop',
     y: 150,
-    connectors: [
+    from: [
       {
         type: 'develop',
-        y: 200,
-      },
-      {
-        type: 'featureTwo',
-        y: 190,
-      },
-      {
-        type: 'featureOne',
-        y: 180,
+        y: 100,
       },
     ],
   },
@@ -48,10 +31,10 @@ export default [
     y: 180,
     popup: true,
     content: 'Major feature for next release',
-    connectors: [
+    from: [
       {
-        type: 'featureOne',
-        y: 240,
+        type: 'develop',
+        y: 150,
       },
     ],
   },
@@ -60,10 +43,10 @@ export default [
     y: 190,
     popup: true,
     content: 'Feature for future release',
-    connectors: [
+    from: [
       {
-        type: 'featureTwo',
-        y: 220,
+        type: 'develop',
+        y: 150,
       },
     ],
   },
@@ -72,14 +55,10 @@ export default [
     y: 170,
     popup: true,
     content: 'Severe bug fixed for production: hotfix 0.2',
-    connectors: [
-      {
-        type: 'develop',
-        y: 200,
-      },
+    from: [
       {
         type: 'master',
-        y: 220,
+        y: 50,
       },
     ],
   },
@@ -88,20 +67,24 @@ export default [
     y: 200,
     popup: true,
     content: 'Incorporate bugfix in develop',
-    connectors: [
+    from: [
+      {
+        type: 'hotfix',
+        y: 170,
+      },
       {
         type: 'develop',
-        y: 250,
+        y: 150,
       },
     ],
   },
   {
     type: 'featureTwo',
-    y: 220,
-    connectors: [
+    y: 225,
+    from: [
       {
-        type: 'develop',
-        y: 250,
+        type: 'featureTwo',
+        y: 190,
       },
     ],
   },
@@ -110,34 +93,38 @@ export default [
     y: 220,
     popup: true,
     content: 'Tag 0.2',
-    connectors: [
+    from: [
       {
         type: 'master',
-        y: 470,
+        y: 50,
+      },
+      {
+        type: 'hotfix',
+        y: 170,
       },
     ],
   },
   {
     type: 'featureOne',
     y: 240,
-    connectors: [
+    from: [
       {
         type: 'featureOne',
-        y: 370,
+        y: 180,
       },
     ],
   },
   {
     type: 'develop',
     y: 250,
-    connectors: [
+    from: [
       {
-        type: 'release',
-        y: 290,
+        type: 'featureTwo',
+        y: 225,
       },
       {
         type: 'develop',
-        y: 370,
+        y: 200,
       },
     ],
   },
@@ -146,7 +133,41 @@ export default [
     y: 290,
     popup: true,
     content: 'Start of release branch for 1.0. From this point on next release means the release after 1.0',
-    connectors: [
+    from: [
+      {
+        type: 'develop',
+        y: 250,
+      },
+    ],
+  },
+  {
+    type: 'release',
+    y: 340,
+    from: [
+      {
+        type: 'release',
+        y: 290,
+      },
+    ],
+  },
+  {
+    type: 'featureOne',
+    y: 370,
+    from: [
+      {
+        type: 'featureOne',
+        y: 240,
+      },
+    ],
+  },
+  {
+    type: 'develop',
+    y: 370,
+    from: [
+      {
+        type: 'develop',
+        y: 250,
+      },
       {
         type: 'release',
         y: 340,
@@ -155,61 +176,23 @@ export default [
   },
   {
     type: 'release',
-    y: 340,
-    connectors: [
-      {
-        type: 'develop',
-        y: 370,
-      },
-      {
-        type: 'release',
-        y: 410,
-      },
-    ],
-  },
-  {
-    type: 'featureOne',
-    y: 370,
-    connectors: [
-      {
-        type: 'featureOne',
-        y: 530,
-      },
-    ],
-  },
-  {
-    type: 'develop',
-    y: 370,
-    connectors: [
-      {
-        type: 'develop',
-        y: 500,
-      },
-      {
-        type: 'featureTwo',
-        y: 430,
-      },
-    ],
-  },
-  {
-    type: 'release',
-    y: 410,
+    y: 395,
     popup: true,
     content: 'Only bugfixes',
-    connectors: [
+    from: [
       {
         type: 'release',
-        y: 450,
+        y: 340,
       },
     ],
   },
   {
     type: 'featureTwo',
     y: 430,
-    connectors: [
+    from: [
       {
-        type: 'featureTwo',
-        y: 480,
+        type: 'develop',
+        y: 370,
       },
     ],
   },
@@ -218,14 +201,10 @@ export default [
     y: 450,
     popup: true,
     content: 'Bugfixes from release branch may be continuously merged back into develop',
-    connectors: [
+    from: [
       {
-        type: 'master',
-        y: 470,
-      },
-      {
-        type: 'develop',
-        y: 500,
+        type: 'release',
+        y: 395,
       },
     ],
   },
@@ -234,17 +213,73 @@ export default [
     y: 470,
     popup: true,
     content: 'Tag 1.0',
-    connectors: [
+    from: [
+      {
+        type: 'release',
+        y: 450,
+      },
       {
         type: 'master',
-        y: 620,
+        y: 220,
       },
     ],
   },
   {
     type: 'featureTwo',
     y: 480,
-    connectors: [
+    from: [
+      {
+        type: 'featureTwo',
+        y: 430,
+      },
+    ],
+  },
+  {
+    type: 'develop',
+    y: 500,
+    from: [
+      {
+        type: 'release',
+        y: 450,
+      },
+      {
+        type: 'develop',
+        y: 370,
+      },
+    ],
+  },
+  {
+    type: 'featureTwo',
+    y: 520,
+    from: [
+      {
+        type: 'featureTwo',
+        y: 480,
+      },
+    ],
+  },
+  {
+    type: 'featureOne',
+    y: 530,
+    from: [
+      {
+        type: 'featureOne',
+        y: 370,
+      },
+    ],
+  },
+  {
+    type: 'develop',
+    y: 570,
+    from: [
+      {
+        type: 'develop',
+        y: 500,
+      },
+      {
+        type: 'featureOne',
+        y: 530,
+      },
       {
         type: 'featureTwo',
         y: 520,
@@ -252,69 +287,41 @@ export default [
     ],
   },
   {
-    type: 'develop',
-    y: 500,
-    connectors: [
+    type: 'release',
+    y: 600,
+    from: [
       {
         type: 'develop',
-        y: 550,
-      },
-    ],
-  },
-  {
-    type: 'featureTwo',
-    y: 520,
-    connectors: [
-      {
-        type: 'develop',
-        y: 550,
-      },
-    ],
-  },
-  {
-    type: 'featureOne',
-    y: 530,
-    connectors: [
-      {
-        type: 'develop',
-        y: 550,
-      },
-    ],
-  },
-  {
-    type: 'develop',
-    y: 550,
-    connectors: [
-      {
-        type: 'develop',
-        y: 600,
-      },
-      {
-        type: 'release',
         y: 570,
       },
     ],
   },
   {
-    type: 'release',
-    y: 570,
-    connectors: [
+    type: 'develop',
+    y: 625,
+    from: [
       {
         type: 'develop',
-        y: 600,
+        y: 570,
       },
       {
-        type: 'master',
-        y: 620,
+        type: 'release',
+        y: 600,
       },
     ],
   },
   {
-    type: 'develop',
-    y: 600,
-  },
-  {
     type: 'master',
-    y: 620,
+    y: 650,
+    from: [
+      {
+        type: 'release',
+        y: 600,
+      },
+      {
+        type: 'master',
+        y: 470,
+      },
+    ],
   },
 ];
